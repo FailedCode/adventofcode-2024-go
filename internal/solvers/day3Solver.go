5c0395499f8895a5bbdbd0c11e895b813f4e58ae
FailedCode <xeres666@googlemail.com>
2024-12-08 17:44:03 +0100

    day 3 part 1

---
+++ internal/solvers/day3Solver.go
@@ -0,0 +1,33 @@
+package solvers
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"internal/utility"
+)
+
+type Day3Solver struct {
+	Day uint
+	InputSource string
+}
+
+func (s Day3Solver) Part1() string {
+	input := utility.LoadInput(s.Day, s.InputSource)
+	r := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	var result int = 0
+	for _, line := range input {
+		matches := r.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			n1, _ := strconv.Atoi(match[1])
+			n2, _ := strconv.Atoi(match[2])
+			result += n1 * n2
+		}
+	}
+
+	return fmt.Sprintf("%v", result)
+}
+
+func (s Day3Solver) Part2() string {
+	return fmt.Sprintf("todo: implement Part2")
+}
