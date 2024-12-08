e7050c91a5e800e29bba440b4299b3dca745a4e6
FailedCode <xeres666@googlemail.com>
2024-12-08 12:54:46 +0100

    day 2 part 2

--- internal/solvers/day2Solver.go
+++ internal/solvers/day2Solver.go
@@ -20,7 +20,7 @@ func (s Day2Solver) Part1() string {
 		}
 		report := lineToNumberSlice(line)
 		// fmt.Println(report)
-		if isReportSafe(report) {
+		if isReportSafe(report, 0) {
 			safeReports += 1
 		}
 	}
@@ -29,27 +29,56 @@ func (s Day2Solver) Part1() string {
 }
 
 func (s Day2Solver) Part2() string {
-	return "todo: implement Part2"
+
+	input := utility.LoadInput(2)
+
+	safeReports := 0
+	for _, line := range input {
+		if len(line) == 0 {
+			break
+		}
+		report := lineToNumberSlice(line)
+		// fmt.Println(report)
+		if isReportSafe(report, 1) {
+			safeReports += 1
+		} else if isReportSafe(report[1:], 0) {
+			// in case the report is something like [1 3 2 1]
+			safeReports += 1
+		}
+	}
+
+	return fmt.Sprintf("%v", safeReports)
 }
 
-func isReportSafe(report []int) bool {
+func isReportSafe(report []int, dampening int) bool {
 	last_n := report[0]
 	sign := 0
-	for _, n := range report[1:] {
+	for position, n := range report[1:] {
 		diff := utility.Abs(last_n - n)
 		if diff < 1 || diff > 3 {
 			// fmt.Printf("diff: %v - %v = %v\n", last_n, n, diff)
+			if dampening > 0 {
+				fixedReport := make([]int, 0, len(report)-1)
+				fixedReport = append(fixedReport, report[:position+1]...)
+				fixedReport = append(fixedReport, report[position+2:]...)
+				return isReportSafe(fixedReport, dampening-1)
+			}
 			return false
 		}
 		newSign := utility.Sgn(last_n - n)
 		if sign != 0 && newSign != sign {
 			// fmt.Printf("direction change: %v => %v (%v => %v)\n", sign, newSign, last_n, n)
+			if dampening > 0 {
+				fixedReport := make([]int, 0, len(report)-1)
+				fixedReport = append(fixedReport, report[:position+1]...)
+				fixedReport = append(fixedReport, report[position+2:]...)
+				return isReportSafe(fixedReport, dampening-1)
+			}
 			return false
 		}
 		sign = newSign
 		last_n = n
 	}
-
 	return true
 }
 
