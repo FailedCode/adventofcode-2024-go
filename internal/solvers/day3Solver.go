166be45209a80d56e591b6b1c9386b183cd41e5e
FailedCode <xeres666@googlemail.com>
2024-12-08 17:58:03 +0100

    day 3 part 2

--- internal/solvers/day3Solver.go
+++ internal/solvers/day3Solver.go
@@ -29,5 +29,29 @@ func (s Day3Solver) Part1() string {
 }
 
 func (s Day3Solver) Part2() string {
-	return fmt.Sprintf("todo: implement Part2")
+	input := utility.LoadInput(s.Day, s.InputSource)
+	r := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
+	var result int = 0
+	var sum_enabled bool = true
+	for _, line := range input {
+		matches := r.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			if match[1] != "" && match[2] != "" {
+				if sum_enabled {
+					n1, _ := strconv.Atoi(match[1])
+					n2, _ := strconv.Atoi(match[2])
+					result += n1 * n2
+				}
+			} else {
+				if match[0] == "do()" {
+					sum_enabled = true
+				}
+				if match[0] == "don't()" {
+					sum_enabled = false
+				}
+			}
+		}
+	}
+
+	return fmt.Sprintf("%v", result)
 }
